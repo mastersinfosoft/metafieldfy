3a4a735b9e101fe1239e083869a87416c607426d
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-03-30 18:26:57 +0530

    add oauth

--- oauth.php
+++ oauth.php
@@ -7,6 +7,7 @@
 	$_GET['signature'] = $_GET['hmac'];
 	echo '<pre>';
 	print_r($_GET);
+	echo '</pre>';
 	# Guard: http://docs.shopify.com/api/authentication/oauth#verification
 		shopify\is_valid_request($_GET, SHOPIFY_APP_SHARED_SECRET) or die('Invalid Request! Request or redirect did not come from Shopify');
 
