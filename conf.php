5e706bfa68f10e8adc54114227b155e2d7bf0de3
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-03-30 17:51:48 +0530

    add oauth

--- conf.php
+++ conf.php
@@ -1,4 +1,4 @@
 <?php
 
 	define('SHOPIFY_APP_API_KEY', 'cc1e527e82daf80ecbe7b1db206c4345');
-	define('SHOPIFY_APP_SHARED_SECRET', 'fd2aa63492c9aea7d0be2fa264cc2a22');
+	define('SHOPIFY_APP_SHARED_SECRET', '016be622f2eace724c6c0cd09247f52f');
