ff9b9b0be222767a048b834fdcff2cb3badb76a4
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-03-30 18:46:42 +0530

    add oauth

--- oauth.php
+++ oauth.php
@@ -1,11 +1,12 @@
 <?php
 	session_start();
-	require '/vendor/autoload.php';
+	require __DIR__.'/vendor/autoload.php';
 	use phpish\shopify;
 
-	require '/conf.php';
+	require __DIR__.'/conf.php';
+	$_GET['signature'] = $_GET['hmac'];
 	# Guard: http://docs.shopify.com/api/authentication/oauth#verification
-		shopify\is_valid_request($_GET, SHOPIFY_APP_SHARED_SECRET) or die('Invalid Request! Request or redirect did not come from Shopify');
+	shopify\is_valid_request($_GET, SHOPIFY_APP_SHARED_SECRET) or die('Invalid Request! Request or redirect did not come from Shopify');
 
 
 	# Step 2: http://docs.shopify.com/api/authentication/oauth#asking-for-permission
