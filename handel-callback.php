373d95d896be6b8cc35568a7cbe69d2f0b8efade
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-03-31 14:33:00 +0530

    adding now database platform

--- handel-callback.php
+++ handel-callback.php
@@ -6,8 +6,8 @@ if(!empty($_GET['shop']) && !empty($_GET['code'])){
 
   $shop = $_GET['shop']; //shop name
   $select_settings = $db->query("SELECT * FROM tbl_appsettings WHERE id = 2");
-  $app_settings = $select_settings->fetch_object();
-
+  $app_settings = $select_settings->fetchObject();
+  
   //get permanent access token
   $access_token = shopify_api\oauth_access_token(
       $_GET['shop'], $app_settings->api_key, $app_settings->shared_secret, $_GET['code']
