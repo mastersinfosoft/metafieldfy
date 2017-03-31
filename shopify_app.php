aef50abc0f8f5e6c873408ce299e5d3ce70846f8
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-03-31 18:08:03 +0530

    adding now database platform

--- shopify_app.php
+++ shopify_app.php
@@ -31,32 +31,31 @@ if(!empty($_GET['shop'])){ //check if the shop name is passed in the URL
   $api_key = $app_settings[0]['api_key'];
   $shared_secret = $app_settings[0]['shared_secret'];
   $data = shopify_api\oauth_access_token($shop, $api_key, $shared_secret, $code);
+  $insert_query = "insert into tbl_usersettings (access_token, store_name, code) values('".$data."', '".$shop."','".$code."')";
+  $insert_store = $db->query($insert_query);
+  $_SESSION['shop'] = $shop;
+  header('Location: https://metafieldfy.herokuapp.com/shopify_testing/admin.php'); //redirect to the admin page
   
-  echo '<pre>';
-  print_r($_GET);
-  print_r($data);
-  echo '</pre>';
-  die();
-  $select_store = $db->query("SELECT store_name FROM tbl_usersettings WHERE store_name = '$shop'"); //check if the store exists
- 
-  if($select_store->rowCount() > 0){
-      
-          $_SESSION['shopify_signature'] = $_GET['signature'];
-          $_SESSION['shop'] = $shop;
-          header('Location: https://metafieldfy.herokuapp.com/shopify_testing/admin.php'); //redirect to the admin page
-      
-      
-  }else{
-
-  }
 
 }else{     
-      //convert the permissions to an array
-      $permissions = json_decode($app_settings[0]['permissions'], true);
-      //get the permission url
-      $scope = empty($permissions) ? '' : '&scope='.implode(',', $permissions);
-      $redirect_uri = empty($app_settings[0]['redirect_url']) ? '' : '&redirect_uri='.urlencode($app_settings[0]['redirect_url']);
-      $permission_url = "https://".$_GET['shop']."/admin/oauth/authorize?client_id=".$app_settings[0]['api_key']."$scope$redirect_uri";
-      header('Location: ' . $permission_url); //redirect to the permission url
+      $select_store = $db->query("SELECT store_name FROM tbl_usersettings WHERE store_name = '$shop'"); //check if the store exists
+ 
+      if($select_store->rowCount() > 0){
+          
+              //$_SESSION['shopify_signature'] = $_GET['signature'];
+              $_SESSION['shop'] = $shop;
+              header('Location: https://metafieldfy.herokuapp.com/shopify_testing/admin.php'); //redirect to the admin page
+          
+          
+      }else{
+          //convert the permissions to an array
+            $permissions = json_decode($app_settings[0]['permissions'], true);
+            //get the permission url
+            $scope = empty($permissions) ? '' : '&scope='.implode(',', $permissions);
+            $redirect_uri = empty($app_settings[0]['redirect_url']) ? '' : '&redirect_uri='.urlencode($app_settings[0]['redirect_url']);
+            $permission_url = "https://".$_GET['shop']."/admin/oauth/authorize?client_id=".$app_settings[0]['api_key']."$scope$redirect_uri";
+            header('Location: ' . $permission_url); //redirect to the permission url
+      }
+      
   }
 }
