514907577c203e3118d33c3f7cd71a60d8000556
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-03-31 18:59:09 +0530

    adding now database platform

--- shopify/admin.php
+++ shopify/admin.php
@@ -1,4 +1,27 @@
-<?php 
-echo "this is my first app thank for visiting";
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+	<h2>Product Metafields for Shopify</h2>
+	<div class="row">
+		<div class="col-sm-3 col-md-3">
+			
+			<div class="list-group">
+			  <a href="javascript:" class="list-group-item active">Add Meta fields</a>
+			  <a href="javascript:" class="list-group-item">Products</a>
+			</div>
+		</div>
+		<div class="col-sm-9 col-md-9">Working area</div>
+		<div></div>
+	</div>
+</div>
+</body>
+</html>
