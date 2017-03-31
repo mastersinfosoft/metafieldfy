789bbcac707ecdf04ccdf684990a66fdff260fe3
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-03-31 11:53:39 +0530

    add app

--- config.php
+++ config.php
@@ -2,4 +2,4 @@
 $db = new Mysqli;
 $db = mysqli_init();
 $db->ssl_set('key.pem', 'cert.pem', 'ca.pem', null, null);
-$db->real_connect(("us-cdbr-iron-east-03.cleardb.net", "b3bd7eeacd8f20", "71ea49b9", "heroku_30e732ce0560b1b");
+$db->real_connect(("us-cdbr-iron-east-03.cleardb.net", "b3bd7eeacd8f20", "71ea49b9", "heroku_30e732ce0560b1b") or die('not able to connect');
