74f47330e2a88cae664d470b9859e7d8f0077df6
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-03-31 11:45:10 +0530

    add ssh

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 $db = new Mysqli;
 $db = mysqli_init();
-$db->ssl_set('key.pem', 'b3bd7eeacd8f20-cert.pem', 'ca.pem', null, null);
+$db->ssl_set('key.pem', 'cert.pem', 'ca.pem', null, null);
 $db->real_connect(("us-cdbr-iron-east-03.cleardb.net", "b3bd7eeacd8f20", "71ea49b9", "heroku_30e732ce0560b1b");
