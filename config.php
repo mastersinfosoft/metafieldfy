a3a393c1334063d7367fc8363baadaa4a0209040
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-03-31 11:40:25 +0530

    add ssl

--- config.php
+++ config.php
@@ -1,2 +1,5 @@
 <?php
-$db = new Mysqli("us-cdbr-iron-east-03.cleardb.net", "b3bd7eeacd8f20", "71ea49b9", "heroku_30e732ce0560b1b");
+$db = new Mysqli;
+$db = mysqli_init();
+$db->ssl_set('key.pem', 'b3bd7eeacd8f20-cert.pem', 'ca.pem', null, null);
+$db->real_connect(("us-cdbr-iron-east-03.cleardb.net", "b3bd7eeacd8f20", "71ea49b9", "heroku_30e732ce0560b1b");
