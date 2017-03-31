a484489b34092352a85b6667060c06e23af481be
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-03-31 11:21:21 +0530

    add databse details

--- config.php
+++ config.php
@@ -1,2 +1,2 @@
 <?php
-$db = new Mysqli("localhost", "mastsoft_shopify", "idW+}=$,h756", "mastsoft_shopify");
+$db = new Mysqli("us-cdbr-iron-east-03.cleardb.net", "b3bd7eeacd8f20", "71ea49b9", "heroku_30e732ce0560b1b");
--- test.php
+++ test.php
@@ -4,4 +4,16 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 echo '<pre>';
 echo 'hi';
 print_r($url);
+include 'config.php';
+$query = "CREATE TABLE IF NOT EXISTS `tbl_appsettings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `api_key` varchar(300) DEFAULT NULL,
+  `redirect_url` varchar(300) DEFAULT NULL,
+  `permissions` text,
+  `shared_secret` varchar(300) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;"
+$select_settings = $db->query($query);
+echo 'hi';
+
 ?>
