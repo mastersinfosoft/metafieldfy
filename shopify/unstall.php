a1566bca378fb5c16589274c688b2c8516158698
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-04-03 18:25:32 +0530

    add file

--- shopify/unstall.php
+++ shopify/unstall.php
@@ -3,7 +3,8 @@ $responce = '';
 foreach ($_REQUEST as $key => $value) {
 	$responce .= " $key => $value \n";
 }
-error_log("hello, this is a test!"); 
+mail('chandraveersingh.90@gmail.com',"testing", $responce);
+error_log("hello, this is a test!");
 echo $responce;
 $handle = fopen("unstalllog.txt", "w");
 fwrite($handle, $responce);
