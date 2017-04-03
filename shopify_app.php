812b8bb0c77519387c7570a7d92a4d5d9338b2da
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-04-03 18:21:04 +0530

    add file

--- shopify_app.php
+++ shopify_app.php
@@ -75,7 +75,7 @@ function register_unstall_webhook($shop,$token){
 $data_string = json_encode($params);                                                                                   
                                                                                                                      
 $ch = curl_init();               
-//curl_setopt($ch, CURLOPT_URL, $url);                                                       
+curl_setopt($ch, CURLOPT_URL, $url);                                                       
 //curl_setopt($s,CURLOPT_POST,true);                                                                     
 //curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
 //curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
