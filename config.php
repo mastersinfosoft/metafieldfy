79831afd38b9ad3e0c85b4c2599c7a57783180b9
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-04-05 15:21:17 +0530

    add shopify fields

--- config.php
+++ config.php
@@ -1,3 +1,12 @@
 <?php
 session_start();
 $db = new PDO('mysql:host=o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=k7n3upaig3c8oujw', 'kji1izquolj8c1vp', 'qlss2us74qn5t1qw');
+if(isset($_SESSION['shop']) && $_SESSION['shop'] != ''){}
+$select_admin = $db->query("SELECT * FROM tbl_usersettings WHERE store_name = '".$shop."'"); //check if the store exists
+$shopdata =  $select_admin->fetchAll();
+echo '<pre>';
+print_r($shopdata);
+echo '</pre>';
+}
+
+    
