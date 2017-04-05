acb430e7a6c91db9ae0f514eba1885288f1d0ee1
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-04-05 18:06:01 +0530

    add files

--- config.php
+++ config.php
@@ -4,9 +4,7 @@ $db = new PDO('mysql:host=o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.
 if(isset($_SESSION['shop']) && $_SESSION['shop'] != ''){
 $select_admin = $db->query("SELECT * FROM tbl_usersettings WHERE store_name = '".$_SESSION['shop']."'"); //check if the store exists
 $shopdata =  $select_admin->fetchAll();
-echo '<pre>';
-print_r($shopdata[0]['access_token']);
-echo '</pre>';
+
 }
 
     
