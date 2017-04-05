2a4c55a46a632610b9729702bb7402c917cf5beb
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-04-05 11:47:13 +0530

    add shopify fields

--- shopify/sidebar.php
+++ shopify/sidebar.php
@@ -1,5 +1,5 @@
 
 <div class="list-group">
   <a href="allmetafields.php" class="list-group-item active">Meta fields Configuration</a>
-  <a href="javascript:" class="list-group-item">Add Meta fields</a>
+  <a href="addmetafields.php" class="list-group-item">Add Meta fields</a>
 </div>
