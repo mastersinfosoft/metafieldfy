cfb69109f5a6e1015d16e91f760f5f9a4a7906ba
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-04-06 13:11:26 +0530

    add file

--- shopify/sidebar.php
+++ shopify/sidebar.php
@@ -1,7 +1,7 @@
 <?php 
-echo $_SERVER['REQUEST_URI'];
+$page =  end(explode('/', $_SERVER['REQUEST_URI']));
 ?>
 <div class="list-group">
-  <a href="allmetafields.php" class="list-group-item active">Meta fields Configuration</a>
-  <a href="addmetafields.php" class="list-group-item">Add Meta fields</a>
+  <a href="allmetafields.php" class="list-group-item <?php if($page == 'allmetafields.php'){ echo 'active'; } ?>">Meta fields Configuration</a>
+  <a href="addmetafields.php" class="list-group-item <?php if($page == 'addmetafields.php'){ echo 'active'; } ?>">Add Meta fields</a>
 </div>
