13474ddaca04abdca619305909a601516a6d51e4
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-04-06 13:09:00 +0530

    add file

--- shopify/sidebar.php
+++ shopify/sidebar.php
@@ -1,4 +1,6 @@
-
+<?php 
+echo $_SERVER['REQUEST_URI'];
+?>
 <div class="list-group">
   <a href="allmetafields.php" class="list-group-item active">Meta fields Configuration</a>
   <a href="addmetafields.php" class="list-group-item">Add Meta fields</a>
