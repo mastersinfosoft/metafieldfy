0baeba0cf3de4c20752c7f18010fe87b567ac02a
chandraveer singh rathore <chandraveersingh.90@gmail.com>
2017-04-07 16:32:37 +0530

    get product form metaforld form

--- shopify/sidebar.php
+++ shopify/sidebar.php
@@ -3,5 +3,5 @@ $page =  end(explode('/', $_SERVER['REQUEST_URI']));
 ?>
 <div class="list-group">
   <a href="allmetafields.php" class="list-group-item <?php if($page == 'allmetafields.php'){ echo 'active'; } ?>">Meta fields Configuration</a>
-  <a href="addmetafields.php" class="list-group-item <?php if($page == 'addmetafields.php'){ echo 'active'; } ?>">Add Meta fields</a>
+  <a href="addmetafields.php" class="list-group-item <?php if($page == 'addmetafields.php'){ echo 'active'; } ?>">Add Product Meta fields</a>
 </div>
